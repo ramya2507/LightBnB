5bcea8e029f25110144cbdac12df16ee46a3cbe3
Ramya Adimoolam <ramyaa1431@gmail.com>
2021-01-08 03:28:02 +0000

    add features to get all reservations of a user

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -62,7 +62,15 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT properties.*, reservations.start_date, reservations.end_date, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.start_date, reservations.end_date
+  ORDER BY reservations.start_date LIMIT $2;`, [guest_id, limit])
+  .then(res => res.rows)
+  .catch(err => console.log(err));
 }
 exports.getAllReservations = getAllReservations;
 
